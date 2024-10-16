4af832a3b78d13692e52410163f3dcd0499e3da5
juatriesp <jtriesp073@iesalmudeyne.es>
2024-10-16 08:57:48 +0200

    Hola Mundo con colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,7 @@
 public class HolaMundo {
     public static void main (String[] args) {
+        String rojo = "\033[32m";
+        String azul = "\033[34m";
         System.out.println("¡Hola Mundo!");
         System.out.println("Aquí estamos haciendo pruebas en Git.");
     }
